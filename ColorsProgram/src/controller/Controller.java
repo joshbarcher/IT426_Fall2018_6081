4f6f5c2ade43f21f9796f9cc8442b087dbdb415f
Josh Archer <jarcher@greenriver.edu>
2018-10-09 14:15:38 -0700

    Added the files from our MVC video lecture.

---
+++ ColorsProgram/src/controller/Controller.java
@@ -0,0 +1,53 @@
+package controller;
+
+import javafx.scene.paint.Color;
+import model.FileData;
+import model.IColorsData;
+import model.ColorPair;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.util.List;
+
+public class Controller
+{
+    private IColorsData model;
+
+    public Controller()
+    {
+        model = new FileData("files/colors.dat"); //new InMemoryData();
+    }
+
+    private String getPath(String file)
+    {
+        try
+        {
+            return new File(file).toURI().toURL().toString();
+        }
+        catch (MalformedURLException ex)
+        {
+            throw new IllegalStateException("File not found");
+        }
+    }
+
+    public boolean handleNewColor(String name, Color color)
+    {
+        if (isEmpty(name) || color == null)
+        {
+            return false;
+        }
+
+        model.addColor(name, color);
+        return true;
+    }
+
+    private boolean isEmpty(String value)
+    {
+        return value == null || value.equals("");
+    }
+
+    public List<ColorPair> handleGetColors()
+    {
+        return model.getColors();
+    }
+}
